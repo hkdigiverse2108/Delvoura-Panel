3a5caa25535354a61743fb88a4909fde9fad35a1
HK-Bhupendra <bhupendrahkdigiverse@gmail.com>
2026-03-17 14:00:03 +0530

    make a redux setup .

---
+++ src/Pages/Auth/SignInForm.tsx
@@ -0,0 +1,10 @@
+
+const SignInForm = () => {
+  return (
+    <div>
+        <h1>Sign in form</h1>
+    </div>
+  )
+}
+
+export default SignInForm
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,18 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { Provider } from 'react-redux'
+import Store from './Store/Store.ts'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <Provider store={Store}>
+        <App />
+      </Provider>
+    </QueryClientProvider>
   </StrictMode>,
 )
